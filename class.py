5ef54804f4bf87d4a34943a3ba69f2dca1b6436e
Grace M codes <gracemcodes@gmail.com>
2024-12-14 18:28:20 +0300

    Made my first commit , with making the first class.

---
+++ class.py
@@ -0,0 +1,7 @@
+class Smartphone:
+    def __init__(self, model, os, battery_life, storage_capacity):
+        # Constructor to initialize the attributes
+        self.model = model
+        self.os = os
+        self.battery_life = battery_life  # in hours
+        self.storage_capacity = storage_capacity  # in GB
